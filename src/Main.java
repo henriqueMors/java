15c61c8e190bb15709a72dbbe010ea0ad098017e
Henrique Moraes <ahmdesouza@gmail.com>
2023-05-19 16:23:50 -0300

    create main
    criado o main para estudo java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+public class Main {
+    public static void main(String[] args) {
+        int val1 = 10;
+        int val2 = 25;
+        double total = val1 +val2;
+        System.out.println(total);
+
+    }
+}
