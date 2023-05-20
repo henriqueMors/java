b76d2076cd1c2345fe4fab81ded17e51aab5cd09
Henrique Moraes <ahmdesouza@gmail.com>
2023-05-20 13:27:28 -0300

    UPDATE JAVA
    inserindo exercicios de fixacao

---
+++ src/Exercicio.java
@@ -0,0 +1,17 @@
+package src;
+public class Exercicio {
+    public static void exercicio (String[] args) {
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+        
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,38 @@
 package src;
 
+import java.util.Locale; //importa a formacao de determinada regiao
+
 // tudo desenvolvido no programa, vai dentro dessa class
 public class Main {
     public static void main(String[] args) {
-        // byte / short / int / long / sao formas de declara uma variavel numerica
+        // byte / short / int / long / sao formas de declarar uma variavel numerica
         // float / double sao numeros com ponto flutuante
         // boolean / valor verdadeiro
         // char / valor caracter
-        byte val1 = 10;
-        short val2 = 25;
-        int total = val1 +val2;
-        System.out.println(total);
+        int val1 = 10;
+        double val2 = 25.3456566;
+        double total = val1 +val2;
+        System.out.println(total);//System.out.println adiciona uma quebra de linha, System.out.print escreve na mesma linha
+        System.out.printf("%.2f%n",total); // printF - formata o numero para quantidade de casas decimais
+        Locale.setDefault(Locale.US);  //formatado para ponto e nao virgula
+        System.out.printf("%.2f%n",total); //locale deve ficar antes da saida de dados
+        /* 
+        //CONCATENANDO
+        
+        System.out.println("Resultado = " + total + " Metros");
+        System.out.printf("Resultado = %2.f Metros%n", total); // utilizando printf para formatar com concatenacao
 
-        int y = 2;
-        System.out.println(y+2);
+        /*
+         * %f = ponto flutuante
+         * %d = inteiro
+         * %s = string
+         * %n = quebra de linha
+         */
 
+         String nome = "Maria";
+         int idade = 25;
+         double renda = 4000.00;
+         System.out.printf("%s ganha R$ %.2f tendo apenas %d anos de idade!%n", nome , renda, idade);
+         
     }
 }
