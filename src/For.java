a373c0f8f2d3d2081ecc54d694c89764413606ff
Henrique Moraes <ahmdesouza@gmail.com>
2023-05-23 11:18:07 -0300

    Create For.java

---
+++ src/For.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Scanner;
+
+public class For {
+ 
+    public static void main (String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        for (int i=0 ; i <= x; i++) {
+            System.out.println(i);
+
+        }
+           
+        sc.close();
+
+    }
+}
