0241b7f7d6d2bdfb6a7f4dd5910b6822e5e38b57
Henrique Moraes <ahmdesouza@gmail.com>
2023-05-26 22:15:15 -0300

    Update TesteDiario.java

--- src/TesteDiario.java
+++ src/TesteDiario.java
@@ -2,21 +2,47 @@ package src;
 
 import java.util.Scanner;
 
-// Criar programa onde vc insere o nome, idade e ele te retorna mensagem do dia.
 public class TesteDiario {
 
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
 
+        char resp;
         String nome;
         int idade;
 
-        nome = sc.next();
-        idade = sc.nextInt();
+        do
+        
+        {
+            System.out.println("Digite seu nome: ");
+            nome = sc.next();
 
-        System.out.printf("Olá %s, você tem %d anos e já está na hora de mudar de ares!%n", nome, idade);
+            System.out.printf("Você digitou que seu nome é " + nome + ", está correto? ( s | n ) -> ");
+            resp = sc.next().charAt(0);
+        }
+        
+        while
+        
+        (resp == 'n');
+
+        do
+        
+        {
+            System.out.println("Qual sua idade: ");
+            idade = sc.nextInt();
+
+            System.out.printf("Sua idade é " + idade + " anos? ( s | n ) -> ");
+            resp = sc.next().charAt(0);
+        }
+        
+        while
+        
+        (resp == 'n');
+
+        System.out.printf("Ok, Seu nome é " + nome + " e sua idade é de " + idade + " anos %n");
 
         sc.close();
+
     }
-    
+
 }
