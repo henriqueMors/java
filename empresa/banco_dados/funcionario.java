eb2af921a1673ca88530dc005d2c2d1b4808d5db
Henrique Moraes <ahmdesouza@gmail.com>
2023-05-30 23:37:37 -0300

    update

--- empresa/banco_dados/funcionario.java
+++ empresa/banco_dados/funcionario.java
@@ -3,15 +3,22 @@ package empresa.banco_dados;
 import java.util.Locale;
 import java.util.Scanner;
 
+import empresa.programa.salario;
+
 public class funcionario {
     public static void main (String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite o nome do funcionario: ");
-        String nome = sc.next();
-        System.out.printf("O nome do funcionario: %s%n", nome);
+        salario sal = new salario();
 
+        System.out.print("Digite o nome do funcionário: ");
+        String nome = sc.nextLine();
+
+        System.out.printf("Digite o salário de %s: ", nome);
+        sal.sal_bruto = sc.nextDouble();
+
+        System.out.printf("A taxa de %s é de: ", nome, sal.desconto());
 
 
         sc.close();
--- empresa/programa/salario.java
+++ empresa/programa/salario.java
@@ -1,5 +1,21 @@
 package empresa.programa;
 
 public class salario {
-    
+
+    public double sal_bruto;
+    public double imposto;
+    public double porcentagem;
+
+    public double desconto() {
+        return (sal_bruto * porcentagem) / 100;
+    }
+
+    public double sal_liq() {
+        return sal_bruto - desconto();
+
+    }
+    public double aumento() {
+        return sal_liq() * porcentagem;
+
+    }
 }
