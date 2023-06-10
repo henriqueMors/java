b22efffea660ce13c5574beb804c0f982e659322
Henrique Moraes <ahmdesouza@gmail.com>
2023-06-09 23:04:26 -0300

    update

--- GetAndSet/Pessoa.java
+++ GetAndSet/Pessoa.java
@@ -1,38 +1,48 @@
 package GetAndSet;
 
+import java.util.Scanner;
+
 public class Pessoa {
-    private String nome;
-    private int idade;
+    private String nome;  // Atributo privado para armazenar o nome
+    private int idade;    // Atributo privado para armazenar a idade
 
-    // Getter para obter o nome
-    public String getNome() {
-        return nome;
+    public String getNome() {  // Getter para obter o nome
+        return nome;  // Retorna o valor armazenado no atributo nome
     }
 
-    // Setter para definir o nome
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String nome) {  // Setter para definir o nome
+        this.nome = nome;  // Atribui o valor passado como argumento ao atributo nome
     }
 
-    // Getter para obter a idade
-    public int getIdade() {
-        return idade;
+    public int getIdade() {  // Getter para obter a idade
+        return idade;  // Retorna o valor armazenado no atributo idade
     }
 
-    // Setter para definir a idade
-    public void setIdade(int idade) {
-        this.idade = idade;
+    public void setIdade(int idade) {  // Setter para definir a idade
+        this.idade = idade;  // Atribui o valor passado como argumento ao atributo idade
     }
 
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-        
+        Pessoa pessoa = new Pessoa();  // Criação de um objeto da classe Pessoa
+
+        Scanner scanner = new Scanner(System.in);  // Criação de um objeto da classe Scanner para ler as entradas do usuário
+
+        // Solicitando e lendo o nome do usuário
+        System.out.print("Digite o nome: ");
+        String nome = scanner.nextLine();  // Lê uma linha de texto digitada pelo usuário
+
+        // Solicitando e lendo a idade do usuário
+        System.out.print("Digite a idade: ");
+        int idade = scanner.nextInt();  // Lê um número inteiro digitado pelo usuário
+
         // Definindo os valores usando os setters
-        pessoa.setNome("João");
-        pessoa.setIdade(25);
-        
+        pessoa.setNome(nome);  // Define o nome da pessoa com o valor lido
+        pessoa.setIdade(idade);  // Define a idade da pessoa com o valor lido
+
         // Obtendo os valores usando os getters
-        System.out.println("Nome: " + pessoa.getNome());
-        System.out.println("Idade: " + pessoa.getIdade());
+        System.out.println("Nome: " + pessoa.getNome());  // Imprime o nome da pessoa
+        System.out.println("Idade: " + pessoa.getIdade());  // Imprime a idade da pessoa
+
+        scanner.close();  // Fecha o objeto Scanner
     }
 }
