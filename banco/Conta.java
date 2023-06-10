4a6deff66342158f47c97c93d51fe4a024070b8f
Henrique Moraes <ahmdesouza@gmail.com>
2023-06-09 23:16:26 -0300

    Update Conta.java

--- banco/Conta.java
+++ banco/Conta.java
@@ -1,7 +1,6 @@
 package banco;
 
-
-public class Conta{
+public class Conta {
 	private int numero;
 	private String detentor;
 	private double saldo;
@@ -9,12 +8,37 @@ public class Conta{
 	public Conta (int numero, String detentor) {
 		this.numero = numero;
 		this.detentor = detentor;
-
 	}
-	public Conta (int numero, String detentor, double saldo) {
+
+	public Conta (int numero, String detentor, double depostitoInicial) {
 		this.numero = numero;
 		this.detentor = detentor;
-		this.saldo = saldo;
+		Deposito(depostitoInicial);
 	}
-	
+
+	public int getNumero() { //setNumero foi apagado para que o numero nao seja alterado
+		return numero;
+	}
+
+	public String getDetentor() {
+		return detentor;
+	}
+
+	public void setDetentor(String detentor) {
+		this.detentor = detentor;
+	}
+
+	public double getSaldo() { // setSaldo foi apagado para que o saldo seja alterado somente por insercao ou saque
+		return saldo;
+	}
+
+	public void Deposito(double valor) {
+		saldo += valor;
+	}
+
+	public void Saque(double valor) {
+		saldo -= valor + 5.00;
+	}
+
+
 }
