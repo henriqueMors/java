ee58f933a86c8fe3ccac65568a59f07aaba7123a
Henrique Moraes <ahmdesouza@gmail.com>
2023-09-01 13:34:24 -0300

    Create EstCond.java

---
+++ src/EstCond.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.util.Scanner;
+
+public class EstCond {
+
+    public static void main (String [] args) {
+    Scanner sc = new Scanner(System.in);
+
+    Double hora;
+    
+    System.out.println("Informe que horas são:");
+    
+    hora = sc.nextDouble();
+    
+     if ( hora > 06 && hora < 12 ) {
+        System.out.println("Bom dia!");
+     } else if ( hora >= 12 && hora < 18) {
+        System.out.println("Boa tarde!");
+     } else {
+        System.out.println("Boa noite!");
+     }
+
+    sc.close();
+
+    }
+    
+}
