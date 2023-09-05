5745bc4207673591480e39d05c5a19c950cfaaf8
Henrique Moraes <ahmdesouza@gmail.com>
2023-09-04 21:40:39 -0300

    Update DiaDaSemana.java

--- src/DiaDaSemana.java
+++ src/DiaDaSemana.java
@@ -1,44 +1,52 @@
 package src;
-
+import javax.swing.JOptionPane;
 import java.util.Scanner;
 
 public class DiaDaSemana {
     public static void main (String [] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite o número para informar o dia da semana: ");
+        //System.out.println("Digite o número para informar o dia da semana: ");
 
-        int dia = sc.nextInt();
+        int dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o número para informar o dia da semana: ")); //sc.nextInt();
 
         switch (dia) {
             case 1:
-                System.out.println("Domingo");
+                JOptionPane.showMessageDialog(null, "Domingo");
+                //System.out.println("Domingo");
                 break;
             case 2:
-                System.out.println("Segunda-feira");
+            JOptionPane.showMessageDialog(null, "Segunda-feira");
+                //System.out.println("Segunda-feira");
                 break;
             case 3:
-                System.out.println("Terça-feira");
+            JOptionPane.showMessageDialog(null, "Terça-feira");
+                //System.out.println("Terça-feira");
                 break;
             case 4:
-                System.out.println("Quarta-feira");
+            JOptionPane.showMessageDialog(null, "Quarta-feira");
+                //System.out.println("Quarta-feira");
                 break;
             case 5:
-                System.out.println("Quinta-feira");
+            JOptionPane.showMessageDialog(null, "Quinta-feira");
+                //System.out.println("Quinta-feira");
                 break;
             case 6:
-                System.out.println("Sexta-feira");
+            JOptionPane.showMessageDialog(null, "Sexta-feira");
+                //System.out.println("Sexta-feira");
                 break;
             case 7:
-                System.out.println("Sábado");
+            JOptionPane.showMessageDialog(null, "Sábado");
+                //System.out.println("Sábado");
                 break;
             default:
-                System.out.println("Opção inválida. Tente novamente.");
+            JOptionPane.showMessageDialog(null, "##ERR404## NOTFOUND \n Opção Inválida!");
+                //System.out.println("Opção inválida. Tente novamente.");
                 break;
 
         }
 
-        sc.close();
+        //sc.close();
     }
     
 }
