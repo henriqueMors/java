297969334523ec6c37c081460bd7ab2c31a6c85e
Henrique Moraes <ahmdesouza@gmail.com>
2023-09-25 20:02:22 -0300

    create SJFILE
    criado pasta java da sao judas

---
+++ saojudas/aplicacao/LivroDeNotas.java
@@ -0,0 +1,19 @@
+package saojudas.aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LivroDeNotas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("## Bem vindo ao livro de notas ##");
+
+
+        sc.close();
+    }
+
+    public void exibirMensagem() {
+    }
+}
---
+++ saojudas/programa/TesteLivroDeNotas.java
@@ -0,0 +1,11 @@
+package saojudas.programa;
+
+import saojudas.aplicacao.LivroDeNotas;
+
+public class TesteLivroDeNotas {
+    public static void main(String[] args) {
+        
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+    }
+}
