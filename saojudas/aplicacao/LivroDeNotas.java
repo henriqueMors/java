b3bb9c65bcec4eadc48dfee4cd3a075df152355d
Henrique Moraes <ahmdesouza@gmail.com>
2023-09-25 20:49:43 -0300

    update

--- saojudas/aplicacao/LivroDeNotas.java
+++ saojudas/aplicacao/LivroDeNotas.java
@@ -7,13 +7,15 @@ public class LivroDeNotas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        
 
-        System.out.println("## Bem vindo ao livro de notas ##");
+        System.out.println("##~ Bem vindo ao livro de notas ~##");
 
 
         sc.close();
     }
 
-    public void exibirMensagem() {
+    public void exibirMensagem(String nomeDoCurso) {
+        System.out.printf("Bem vindo ao livro de notas do curso %s", nomeDoCurso);
     }
 }
--- saojudas/programa/TesteLivroDeNotas.java
+++ saojudas/programa/TesteLivroDeNotas.java
@@ -1,11 +1,19 @@
 package saojudas.programa;
 
+import java.util.Scanner;
+
 import saojudas.aplicacao.LivroDeNotas;
 
 public class TesteLivroDeNotas {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
         LivroDeNotas livroDeNotas = new LivroDeNotas();
-        livroDeNotas.exibirMensagem();
+        String NomeDoCurso = sc.nextLine();
+        livroDeNotas.exibirMensagem(NomeDoCurso);
+
+        System.out.println("testando");
+
+        sc.close();
     }
 }
