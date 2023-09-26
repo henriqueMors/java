e34fb04a79198c129467e27799074d4bd604b40a
Henrique Moraes <ahmdesouza@gmail.com>
2023-09-26 16:02:17 -0300

    update java

--- Pensao/Aplicação/Pensionato.java
+++ Pensao/Aplicação/Pensionato.java
@@ -2,23 +2,37 @@ package Pensao.Aplicação;
 
 import java.util.Locale;
 import java.util.Scanner;
-
-import Pensao.Programa.Programa;
+import Pensao.Programa.Aluguel;
 
 public class Pensionato {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Quantos estudantes receberemos?");
-        int n = sc.nextInt();
-        Programa [] vect = new Programa [n];
+        Aluguel[] vect = new Aluguel[10];
 
-            for (int i=0; i<n; i++) {
-            String estudante = sc.nextLine();
-            vect[i] = new Programa(estudante);
+        System.out.println("Quantos quartos gostaria de alugar?");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.println("Locatário: #" + i);
+            System.out.print("Qual nome do locatário #" + i + ":");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.print("Qual email do locatário #" + i + ":");
+            String email = sc.next();
+            System.out.print("Quarto: ");
+            int quarto = sc.nextInt();
+
+            vect[quarto] = new Aluguel(nome, email);
         }
 
+        System.out.println("Quartos ocupados");
+        for (int i = 0; i < 10; i++) {
+            if (vect[i] != null){
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
 
         sc.close();
     }
