281c209ff8c4d9e4c7fcbb411ea28243ca710409
Henrique Moraes <ahmdesouza@gmail.com>
2023-11-06 18:34:49 -0300

    Update AluguelDeCarro.java

--- locadora/model_entities/AluguelDeCarro.java
+++ locadora/model_entities/AluguelDeCarro.java
@@ -12,4 +12,46 @@ public class AluguelDeCarro {
     public AluguelDeCarro() {
 
     }
+
+    public AluguelDeCarro(LocalDateTime inicio, LocalDateTime fim, Vehicle vehicle, Fatura fatura) {
+        this.inicio = inicio;
+        this.fim = fim;
+        this.vehicle = vehicle;
+        this.fatura = fatura;
+
+    }
+
+        public LocalDateTime getInicio() {
+        return inicio;
+    }
+
+        public void setInicio(LocalDateTime inicio) {
+        this.inicio = inicio;
+    }
+
+        public LocalDateTime getFim() {
+        return fim;
+    }
+
+        public void setFim(LocalDateTime fim) {
+        this.fim = fim;
+    }
+
+        public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+        public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+        public Fatura getFatura() {
+        return fatura;
+    }
+
+        public void setFatura(Fatura fatura) {
+        this.fatura = fatura;
+    }
+
+
 }
