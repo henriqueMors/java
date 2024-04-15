f706958cacbbbd5b0a38923fd554549084a0ee09
Henrique Moraes <ahmdesouza@gmail.com>
2024-04-15 19:20:47 -0300

    migrando para netbeans

--- iniciandoo/src/Main.java
+++ iniciandoo/src/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         product("", 0);
+
     }
 
     public static void product (String nome, double valor) {
