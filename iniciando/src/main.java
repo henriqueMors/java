16893d69721cc4734502f0d234a0099af429863d
Henrique Moraes <ahmdesouza@gmail.com>
2024-11-27 19:13:29 -0300

    estudando

--- iniciando/src/main.java
+++ iniciando/src/main.java
@@ -1,6 +1,5 @@
 package iniciando.src;
 
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
--- src/TesteDiario.java
+++ src/TesteDiario.java
@@ -1,47 +1,33 @@
 package src;
 
-import java.util.Scanner;
+import java.util.Locale;
 
 public class TesteDiario {
 
     public static void main (String [] args) {
-        Scanner sc = new Scanner(System.in);
 
-        char resp;
-        String nome;
-        int idade;
+        String product1 = "Computer";
+        String product2 = "Office Desk";
 
-        do
-        
-        {
-            System.out.println("Digite seu nome: ");
-            nome = sc.next();
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
 
-            System.out.printf("Você digitou que seu nome é %s, está correto? ( s | n ) -> ", nome);
-            resp = sc.next().charAt(0);
-        }
-        
-        while
-        
-        (resp == 'n');
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
 
-        do
-        
-        {
-            System.out.println("Qual sua idade: ");
-            idade = sc.nextInt();
+        System.out.println("Products:");
+        System.out.printf("%s, which price is R$ %.2f\n", product1, price1);
+        System.out.printf("%s, which price is R$ %.2f\n\n", product2, price2);
 
-            System.out.printf("Sua idade é de %d anos? ( s | n ) -> ", idade);
-            resp = sc.next().charAt(0);
-        }
-        
-        while
-        
-        (resp == 'n');
+        System.out.printf("Record: %d years old, code %d and gender: %s\n\n", age, code, gender);
 
-        System.out.printf("Ok, Seu nome é %s e sua idade é de %d anos! %n", nome, idade);
+        System.out.printf("Measue with eight deciaml places: %.8f\n", measure);
+        System.out.printf("Rouded (trhee decimal places): %.3f\n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f\n", measure);
 
-        sc.close();
 
     }
 
