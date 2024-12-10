3cbe57f97b97fadfc521934304ab074e13237d0d
Henrique Moraes <ahmdesouza@gmail.com>
2024-12-09 23:42:02 -0300

    Update TesteDiario.java

--- src/TesteDiario.java
+++ src/TesteDiario.java
@@ -6,23 +6,10 @@ import java.util.Scanner;
 public class TesteDiario {
 
     public static void main (String [] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
-        char resp;
+        String original = "dfgSETRNM NhGFGF ghf";
 
-        do {
-        System.out.print("Digite o valor em celsius: ");
-        double C = sc.nextDouble();
-        double F = 9.0 * C / 5 + 32;
-
-        System.out.printf("Em F 'e considerada a temperatura de %.1f%n", F);
-        System.out.print("Deseja repetir? (s/n) ");
-        resp = sc.next().charAt(0);
-        } while (resp != 'n');
-
-        System.out.println("Ok! See you!");
-        sc.close();
+        System.out.println(original.trim());
 
     }
 
